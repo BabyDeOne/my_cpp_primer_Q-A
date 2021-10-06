364420199fdac12896396fccd298769320089073
Librarian0ne <hanquanl0115@gmail.com>
2021-10-05 20:42:44 -0400

    20211005

--- 1/exercises/1-11.cpp
+++ 1/exercises/1-11.cpp
@@ -2,18 +2,18 @@
 
 int main(){
     std::cout << "Please enter two numbers: " << std::endl;
-    int number_1 = 0, number_2 = 0, input1 = 0, input2 = 0;
+    int bigNumber = 0, smallNumber = 0, input1 = 0, input2 = 0;
     std::cin >> input1 >> input2;
     if (input1 > input2){
-        number_1 = input1;
-        number_2 = input2;
+        bigNumber = input1;
+        smallNumber = input2;
     }else{
-        number_1 = input2;
-        number_2 = input1;
+        bigNumber = input2;
+        smallNumber = input1;
     }
-    while (number_1 >= number_2){
-        std::cout << number_2 << std::endl;
-        ++number_2;
+    while (bigNumber >= smallNumber){
+        std::cout << smallNumber << std::endl;
+        ++smallNumber;
     }
     return 0;
 }
