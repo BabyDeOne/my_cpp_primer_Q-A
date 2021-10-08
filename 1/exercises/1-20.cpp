92aa6750e44204923d525d12d09a1544feaac8d3
Librarian0ne <hanquanl0115@gmail.com>
2021-10-07 20:06:49 -0400

    20211007

---
+++ 1/exercises/1-20.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "c:/cpp/cppprimer/1/Sales_item.h"
+
+int main(){
+    Sales_item book;
+
+    std::cin >> book ;
+    std::cout << book << std::endl;
+    return 0;
+}
