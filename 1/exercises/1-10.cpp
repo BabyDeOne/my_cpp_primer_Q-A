7bd20de08b6695d0ab704235d8689ec5300f0ada
Librarian0ne <hanquanl0115@gmail.com>
2021-10-14 21:59:44 -0400

    20211014

--- 1/exercises/1-10.cpp
+++ 1/exercises/1-10.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    int count = 10;
+    int count = 20;
     while (count >= 0){
         std::cout << count << std::endl;
         -- count;
---
+++ 1/exercises/1-23.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "/mnt/c/cpp/cppprimer/1/Sales_item.h"
+
+int main(){
+    Sales_item currentIsbn, anotherIsbn;
+    if (std::cin >> currentIsbn){
+        int count = 1;
+        while(std::cin >> anotherIsbn){
+            if(anotherIsbn.isbn() == currentIsbn.isbn()){
+                ++count;
+            }else{
+                std::cout << currentIsbn << " occurs " << count << " times" << std::endl;
+                currentIsbn = anotherIsbn;
+                count = 1;
+            }
+        }
+        std::cout << currentIsbn << " occurs " << count << " times" << std::endl;
+    }
+    return 0;
+}
