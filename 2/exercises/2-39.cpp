04b5e670d1fe859920921ed3c35aa8b2ea865c4c
playerone <hanquanl0115@gmail.com>
2021-12-22 18:50:19 -0500

    20211222

---
+++ 2/exercises/2-39.cpp
@@ -0,0 +1,14 @@
+/*
+    Compile the following program to see what happens when you forget the semicolon after a class
+    definition. Remenber the message for future reference.
+*/
+
+struct Foo {/* empty*/}  //Note: no semicolon
+
+int main(){
+    return 0;
+}
+
+/*
+    error: expected ‘;’ after struct definition
+*/
