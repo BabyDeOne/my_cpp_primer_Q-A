de0094b46d34433df18c965b269e54fd7a99fd56
playerone <hanquanl0115@gmail.com>
2022-06-09 21:55:00 -0400

    20220609

--- 1/exercises/1-16.cpp
+++ 1/exercises/1-16.cpp
@@ -4,7 +4,7 @@ int main(){
     int sum = 0, value = 0;
     while ( std::cin >> value )
         sum += value;
-    std::cout << "sum is: " << sum << std::endl;    
+    std::cout << "sum is: " << sum  <<std::endl;    
     
     return 0;
 }
